cf4ea1ab4db434a0bb4b6c5847e7795a9a25215c
delimajo <joaquim.delima@cba.com.au>
2018-09-24 19:43:07 +1000

    Checkpoint

--- url.py
+++ url.py
@@ -1,34 +1,26 @@
 import requests
-from xml.etree import ElementTree as ET
+import xml.etree.ElementTree as ET
 import re
-import ffmpeg
-import os
+import urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://www.bitchute.com/feeds/rss/channel/rongibson/'
 r = requests.get(url, verify=False)
-root = ET.fromstring(r.text)
-# for child in root[0]:
-for chan in root:
-    # print(child.tag, child.attrib, child.text)
+root = ET.fromstring(r.content)
+
+for chan in root[0]:
     for item in chan.findall('item'):
+        for title in item.findall('title'):
+            old_title = (title.text)
         for link in item.findall('link'):
-            print(link.text)
+            old_link = (link.text)
         for guid in item.findall('guid'):
-            print (guid.text)
-    if child.tag == 'item':
-# it is kinda working like you think up to here.
-        for kid in child.iter():
-            print(index, kid.tag, kid.attrib, kid.text)
-            # if kid.tag == 'link':
-            thetest = kid[index].tag
-            thelink = requests.get(kid[index].text, verify=False)
-            thelinktext = thelink.text
-            thelinklinks = re.findall('"(https:\/\/seed.*?)"', thelinktext)
-            link = thelinklinks[0]
-            # if kid.tag == 'guid':
-            guid = kid[guid].text
-            # if kid.tag == 'enclosure':
-            thumb = kid[guid].attrib[url]
-            print('The link is %s, the guid is %s, the thumb is %s' % link, guid, thumb)
-            # os.system("ffmpeg -i %s %s.mp3 -y" % link, guid)                
+            old_guid = (guid.text)
+        for enclosure in item.findall('enclosure'):
+            thumb_link = (enclosure.attrib['url'])
+            new_link = re.findall('"(https:\/\/seed.*?)"', ((requests.get(old_link, verify=False)).text))
+            enclosure.set('url', new_link[0])
+
+tree = ET.ElementTree(root)
+tree.write("output.rss")
 print('I am at the end now')
