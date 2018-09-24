dc31309ffedf7444489886e9ae67c738bb092832
Joaquim <logins@use.startmail.com>
2018-09-24 22:47:07 +1000

    Checkpoint

--- url.py
+++ url.py
@@ -6,9 +6,10 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://www.bitchute.com/feeds/rss/channel/rongibson/'
 r = requests.get(url, verify=False)
-root = ET.fromstring(r.content)
+r = (requests.utils.get_unicode_from_response(r))
+root = ET.fromstring(r)
 
-for chan in root[0]:
+for chan in root:
     for item in chan.findall('item'):
         for title in item.findall('title'):
             old_title = (title.text)
@@ -20,7 +21,14 @@ for chan in root[0]:
             thumb_link = (enclosure.attrib['url'])
             new_link = re.findall('"(https:\/\/seed.*?)"', ((requests.get(old_link, verify=False)).text))
             enclosure.set('url', new_link[0])
+        for description in item.findall('description'):
+            old_description = (description.text)
+            # new_description = ('<![CDATA[<img src="' + thumb_link + '"/>')
+            # new_description = new_description + old_title
+            # problems with the cdata encoding, figure it out
+            new_description = old_title
+            description.text = new_description
 
 tree = ET.ElementTree(root)
-tree.write("output.rss")
+tree.write("output.rss", encoding="utf-8")
 print('I am at the end now')
