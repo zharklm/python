90a9400bf3b467a088a6a9987064cdb0e46122b5
Joaquim De Lima <joaquim.delima@cba.com.au>
2018-09-25 18:42:19 +1000

    Maybe

--- url2.py
+++ url2.py
@@ -6,7 +6,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = 'https://seed07.bitchute.com/U8pFRoeI7dXM/6a3Iw7suM1TN.mp4'
 r = requests.get(url, stream=True, verify=False)
-infile = ffmpeg.input(r.content)
+infile = ffmpeg.input(r)
 outfile = ffmpeg.output(infile, 'output.mp3')
 ffmpeg.run(outfile)
 r.close()
