862a712f59cdc42e5cae5f77c2a54b84cfcd956d
Joaquim <logins@use.startmail.com>
2018-09-26 01:30:16 +1000

    Reasonable progress

--- url.py
+++ url.py
@@ -3,9 +3,11 @@ import xml.etree.ElementTree as ET
 import re
 import urllib3
 import tinys3
+import io
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = 'https://www.bitchute.com/feeds/rss/channel/rongibson/'
+ET.register_namespace('atom','http://www.w3.org/2005/Atom')
 r = requests.get(url, verify=False)
 r = (requests.utils.get_unicode_from_response(r))
 root = ET.fromstring(r)
@@ -22,6 +24,8 @@ for chan in root:
             thumb_link = (enclosure.attrib['url'])
             new_link = re.findall('"(https:\/\/seed.*?)"', ((requests.get(old_link, verify=False)).text))
             enclosure.set('url', new_link[0])
+        for link in item.findall('link'):
+            link.text = thumb_link
         for description in item.findall('description'):
             old_description = (description.text)
             # new_description = ('<![CDATA[<img src="' + thumb_link + '"/>')
@@ -29,13 +33,11 @@ for chan in root:
             # problems with the cdata encoding, figure it out
             new_description = old_title
             description.text = new_description
-tree_out = open('ron2.rss', 'w+t')
-tree = ET.ElementTree(root)
-tree.write(tree_out, encoding="unicode")
-tree_out.close()
-tree_bin = open('ron2.rss', 'rb')
-# tree_out.write(ET.tostring(root, encoding="unicode"))
+
+tree_out = io.BytesIO(b"abcdef")
+tree_out.write(ET.tostring(root, encoding="utf-8"))
+
 s3_con = tinys3.Connection('cheeky', 'bugger', tls=True)
-s3_con.upload('ron2.rss', tree_bin, 'secrets')
-tree_bin.close()
+s3_con.upload('ron3.rss', tree_out, 'secrets')
+tree_out.close()
 print('I am at the end now')
